3a13f2c99b4a71fed0e60927db5e7d46f9228baa
Nana Quam <quamgyambrah@gmail.com>
2022-11-19 22:14:17 +0000

    writing elegant requests with express

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ app.listen(port, () => {
   console.log(`Express app listening on port ${port}`);
 });
 
-// a sample request
+// a sample requests
 app.get("/", (req, res) => {
-  res.send("Hello Express!");
+  //   res.send("Hello Express!");
+  res.sendFile("views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  //   res.send("Hello Express!");
+  res.sendFile("views/about.html", { root: __dirname });
+});
+
+// redirect
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).sendFile("views/404.html", { root: __dirname });
 });
