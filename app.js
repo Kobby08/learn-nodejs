b6d567717616bb004dfe38775ce5a0fb685dc5f7
Nana Quam <quamgyambrah@gmail.com>
2022-11-20 20:58:01 +0000

    using view engine(ejs)

--- app.js
+++ app.js
@@ -12,15 +12,32 @@ app.listen(port, () => {
   console.log(`Express app listening on port ${port}`);
 });
 
-// a sample requests
+// requests handlers
 app.get("/", (req, res) => {
-  //   res.send("Hello Express!");
-  res.sendFile("views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
   //   res.send("Hello Express!");
-  res.sendFile("views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create Blog" });
 });
 
 // redirect
@@ -30,5 +47,5 @@ app.get("/about-us", (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "Create Blog" });
 });
