2d348659be7363c930452b5b811621faf6f9bba1
Nana Quam <quamgyambrah@gmail.com>
2022-11-20 23:25:32 +0000

    fixes

--- app.js
+++ app.js
@@ -35,27 +35,21 @@ app.set("view engine", "ejs");
 app.use(morgan("dev"));
 app.use(express.static("public"));
 
-// mongoose request handlers
-app.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "Blog 2",
-    snippet: "have a sneek preview",
-    body: "have the actual content",
-  });
-  blog
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+// routes
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
 });
 
+app.get("/about", (req, res) => {
+  //   res.send("Hello Express!");
+  res.render("about", { title: "About" });
+});
+
+// blog routes
 app.get("/blogs", async (req, res) => {
   try {
-    const blogs = await Blog.find();
-    res.send(blogs);
+    const blogs = await Blog.find().sort({ createdAt: -1 });
+    res.render("index", { title: "All Blogs", blogs });
   } catch (error) {
     console.log(error);
   }
@@ -70,48 +64,11 @@ app.get("/blog", async (req, res) => {
   }
 });
 
-// requests handlers
-app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
-});
-
-app.get("/about", (req, res) => {
-  //   res.send("Hello Express!");
-  res.render("about", { title: "About" });
-});
-
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create Blog" });
-});
-
-// redirect
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
 });
 
 // 404 page
 app.use((req, res) => {
   res.status(404).render("404", { title: "Create Blog" });
-});
-
-// middlewares are functions that run in between a req and a res.
-app.use((req, res, next) => {
-  console.log("host: ", req.hostname);
-  console.log("path: ", req.path);
-  console.log("method: ", req.method);
-  next();
 });
